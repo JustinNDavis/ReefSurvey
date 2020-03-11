4cd5009aab2c613225af208931b91d4975f0f7a0
JustinNDavis <59874785+JustinNDavis@users.noreply.github.com>
2020-03-11 11:49:51 -0400

    C# instead of ASP.net
    C# files.  Connected DB

---
+++ ReefSurveyCSharp/ReefSurveyCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReefSurveyCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
